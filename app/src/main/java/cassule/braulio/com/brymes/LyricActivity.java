61040dc791156abd7c6553d4d10b9d73421ff079
Braulio Cassule <brauliocassule94@gmail.com>
2018-02-18 16:57:00 +0100

    make audio player #1

---
+++ app/src/main/java/cassule/braulio/com/brymes/LyricActivity.java
@@ -0,0 +1,145 @@
+package cassule.braulio.com.brymes;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+public class LyricActivity extends AppCompatActivity implements MediaPlayer.OnBufferingUpdateListener,MediaPlayer.OnCompletionListener{
+
+    private ImageButton btn_play_pause;
+    private SeekBar seekBar;
+    private TextView textView;
+
+
+    private MediaPlayer mediaPlayer;
+    private int mediaFileLength;
+    private int realtimeLength;
+    final Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        seekBar = findViewById(R.id.seekbar);
+        seekBar.setMax(99); // 100% (0~99)
+        seekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(mediaPlayer.isPlaying())
+                {
+                    SeekBar seekBar = (SeekBar)v;
+                    int playPosition = (mediaFileLength/100)*seekBar.getProgress();
+                    mediaPlayer.seekTo(playPosition);
+                }
+                return false;
+            }
+        });
+
+        textView = findViewById(R.id.textTimer);
+
+        btn_play_pause = findViewById(R.id.btn_play_pause);
+        btn_play_pause.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog mDialog = new ProgressDialog(LyricActivity.this);
+
+
+                AsyncTask<String,String,String> mp3Play = new AsyncTask<String, String, String>() {
+
+                    @Override
+                    protected void onPreExecute() {
+                        mDialog.setMessage("Please wait");
+                        mDialog.show();
+                    }
+
+                    @Override
+                    protected String doInBackground(String... params) {
+                        try{
+                            mediaPlayer.setDataSource(params[0]);
+                            mediaPlayer.prepare();
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+                        return "";
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+                        mediaFileLength = mediaPlayer.getDuration();
+                        realtimeLength = mediaFileLength;
+                        if(!mediaPlayer.isPlaying())
+                        {
+                            mediaPlayer.start();
+                            btn_play_pause.setImageResource(R.drawable.ic_pause);
+                        }
+                        else
+                        {
+                            mediaPlayer.pause();
+                            btn_play_pause.setImageResource(R.drawable.ic_play);
+                        }
+
+                        updateSeekBar();
+                        mDialog.dismiss();
+                    }
+                };
+
+                mp3Play.execute("http://www.worldhiphopbeats.com/mp3/Hendrix.mp3"); // direct link mp3 file
+            }
+        });
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+
+
+    }
+
+    private void updateSeekBar() {
+        seekBar.setProgress((int)(((float)mediaPlayer.getCurrentPosition() / mediaFileLength)*100));
+        if(mediaPlayer.isPlaying())
+        {
+            Runnable updater = new Runnable() {
+                @Override
+                public void run() {
+                    updateSeekBar();
+                    realtimeLength-=1000; // declare 1 second
+                    textView.setText(String.format("%d:%d",TimeUnit.MILLISECONDS.toMinutes(realtimeLength),
+                            TimeUnit.MILLISECONDS.toSeconds(realtimeLength) -
+                                    TimeUnit.MILLISECONDS.toSeconds(TimeUnit.MILLISECONDS.toMinutes(realtimeLength))));
+
+                }
+
+            };
+            handler.postDelayed(updater,1000); // 1 second
+        }
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mp, int percent) {
+        seekBar.setSecondaryProgress(percent);
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        btn_play_pause.setImageResource(R.drawable.ic_play);
+    }
+}
